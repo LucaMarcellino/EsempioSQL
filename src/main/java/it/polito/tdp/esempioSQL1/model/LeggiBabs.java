b7e2a23ac24ee21e3246157ede9cc995227af6b2
luki_ <luki_@DESKTOP-JF77T7F.station>
2020-04-16 14:27:20 +0200

    Esempio SQL

--- src/main/java/it/polito/tdp/esempioSQL1/model/LeggiBabs.java
+++ src/main/java/it/polito/tdp/esempioSQL1/model/LeggiBabs.java
@@ -2,6 +2,7 @@ package it.polito.tdp.esempioSQL1.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,9 +14,11 @@ public class LeggiBabs {
 		
 		try {
 			Connection conn = DriverManager.getConnection(jdbcURL);
-			Statement st= conn.createStatement();
-			String SQL="SELECT NAME FROM station";
-			ResultSet res=st.executeQuery(SQL);
+			String SQL="SELECT NAME FROM station WHERE landmark= ? ";
+			
+			PreparedStatement st= conn.prepareStatement(SQL);
+			st.setString(1, "Palo Alto");
+			ResultSet res=st.executeQuery();
 			
 			while(res.next()) {
 				String nomeStazione=res.getString("name");
